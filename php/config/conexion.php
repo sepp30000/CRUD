70809bea26e2352c5ff2a715de8e760d5596d854
sepp30000 <128365404+sepp30000@users.noreply.github.com>
2024-06-11 01:32:21 +0200

    Update conexion.php

--- php/config/conexion.php
+++ php/config/conexion.php
@@ -3,7 +3,7 @@ require 'config.php';
 # creación de la conexión pdo
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "Conexión exitosa a la base de datos";
+#    echo "Conexión exitosa a la base de datos";
 } catch (PDOException $e) {
     echo 'Error de conexión: ' . $e->getMessage();
     die();
